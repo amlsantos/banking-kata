cf945dbdafe21270f203575d91244ac685125ff2
Andre Santos <6472330+santos-an@users.noreply.github.com>
2024-01-05 18:12:12 +0000

    refactor - add date value object

--- src/Domain/Account.cs
+++ src/Domain/Account.cs
@@ -5,44 +5,34 @@ namespace Domain;
 public class Account
 {
     private Money _balance;
-    private DateOnly _date;
+    private Date _date;
 
-    public Account()
-    {
-        _balance = new Money(0);
-    }
-    
+    public Account() => _balance = new Money(0);
+
     public void Deposit(int amount)
     {
         _balance += new Money(amount);
-        
-        _date = new DateOnly(2015, 12, 24);
+        _date = new Date(2015, 12, 24);
     }
-    
+
     public string PrintStatement()
     {
         var statement = new StringBuilder();
 
         var header = "Date".PadRight(12) + "Amount".PadRight(8) + "Balance".PadRight(7);
         statement.Append(header);
-        
+
         if (_balance > 0)
         {
             statement.Append('\n');
             statement.Append(TransactionAsString());
         }
-            
-        
+
         return statement.ToString();
     }
 
     private string TransactionAsString()
     {
-        return $"{DateAsString(_date), -12} {_balance.SignAsString(), -9} {_balance.MoneyAsString(), -8}";
-    }
-    
-    private string DateAsString(DateOnly date)
-    {
-        return $"{date.Day}.{date.Month}.{date.Year}";
+        return $"{_date.AsString(),-12} {_balance.SignAsString(),-9} {_balance.MoneyAsString(),-8}";
     }
 }
---
+++ src/Domain/Date.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain;
+
+public class Date
+{
+    private readonly DateOnly _date;
+
+    public Date(int year, int month, int day)
+    {
+        _date = new DateOnly(year, month, day);
+    }
+
+    public string AsString() => $"{_date.Day}.{_date.Month}.{_date.Year}";
+}
